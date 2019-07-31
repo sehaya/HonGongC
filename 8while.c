cba46d5f532caa631bdd29607b9e044710912b40
sehaya <sehaya@gmail.com>
2019-07-31 20:54:09 +0900

    Update 8while.c

--- 8while.c
+++ 8while.c
@@ -1,9 +1,17 @@
+/***** <혼자 공부하는 C 언어> 232쪽 도전 실전 예제 - 8ch을 while로 풀기 
+
+대소문자 변환 프로그램
+
+키보드로부터 문장을 입력받은 후에 대문자를 찾아 소문자로 바꾸는 프로그램을 작성하세요. 
+바뀐 문장과 바뀐 문자의 수도 함께 출력합니다.
+*****/
+
 #include <stdio.h>
 
 int main(void)
 {
-	char sentence[80]="";
-	int i=0, count=0;
+	char sentence[80] = "";
+	int i = 0, count = 0;
 
 	printf("영어 문장을 입력하세요. :");
 	gets(sentence);
@@ -12,7 +20,7 @@ int main(void)
 	{
 		if (('A' <= sentence[i])&&(sentence[i] < 'a'))
 		{
-			sentence[i] = sentence[i]+32;
+			sentence[i] = sentence[i] + 32;
 			count = count + 1;
 		}
 		i++;
